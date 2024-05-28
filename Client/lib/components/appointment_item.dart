bbf1724d857d7345b3f13ec5544a2f62a5f85ecb
MarkPeter19 <99884822+MarkPeter19@users.noreply.github.com>
2024-05-29 00:40:48 +0300

    Added ViewAppointment UI (not finished yet)

--- Client/lib/components/appointment_item.dart
+++ Client/lib/components/appointment_item.dart
@@ -1,55 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:doctorgpt/services/doctor_services.dart';
+
 
 class AppointmentItem extends StatelessWidget {
   final String doctorId;
   final String message;
   final Map<String, dynamic> date;
   final String hourMinute;
+  final Timestamp? sendTime; // Módosítva, hogy opcionális legyen
   final VoidCallback onTap;
 
   const AppointmentItem({
+    Key? key,
     required this.doctorId,
     required this.message,
     required this.date,
-    required this.hourMinute, // Új paraméter hozzáadása
+    required this.hourMinute,
+    required this.sendTime,
     required this.onTap,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     String formattedDate =
-        '${date['year']}-${date['month']}-${date['day']} ${hourMinute ?? ''}';
+      '${date['year']}-${date['month']}-${date['day']} $hourMinute';
+
+     String? sentAt = sendTime != null
+      ? '${sendTime!.toDate().year}-${sendTime!.toDate().month}-${sendTime!.toDate().day} ${sendTime!.toDate().hour}:${sendTime!.toDate().minute}'
+      : null;
 
     return FutureBuilder(
       future: DoctorServices().getDoctorName(doctorId),
       builder: (context, AsyncSnapshot<String> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator(); // Várakozás jelző megjelenítése
+          return const CircularProgressIndicator();
         }
         if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}'); // Hiba esetén hibaüzenet megjelenítése
+          return Text('Error: ${snapshot.error}');
         }
-        // Sikeres lekérdezés esetén az orvos nevének megjelenítése
         return Card(
-          margin: EdgeInsets.all(8.0),
+          elevation: 2,
+          margin: const EdgeInsets.all(8.0),
           child: ListTile(
             leading: Container(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.green, // Zöld kör háttérszín
+                color: Colors.green,
               ),
-              child: Padding(
+              child: const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Icon(Icons.calendar_today, color: Colors.white), // Naptár ikon
+                child: Icon(Icons.calendar_today, color: Colors.white),
               ),
             ),
-            title: Text(
-              snapshot.data ?? 'Unknown', // Orvos neve
-              style: TextStyle(fontWeight: FontWeight.bold),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  snapshot.data ?? 'Unknown',
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                if (sendTime != null) // Csak akkor jelenítsd meg, ha nem null
+                  Text(
+                    'Sent at: $sentAt',
+                    style: const TextStyle(fontSize: 16, color: Color.fromARGB(255, 122, 121, 121)),
+                  ),
+              ],
             ),
-            subtitle: Text(formattedDate),
-            trailing: Icon(Icons.keyboard_arrow_right),
+            subtitle: Text('Appointment: $formattedDate', style: const TextStyle(fontSize: 16),),
+            trailing: const Icon(Icons.keyboard_arrow_right),
             onTap: onTap,
           ),
         );
--- Client/lib/screens/PatientScreens/Home/ViewAppointmentScreen.dart
+++ Client/lib/screens/PatientScreens/Home/ViewAppointmentScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:doctorgpt/services/doctor_services.dart';
 
 class ViewAppointmentScreen extends StatelessWidget {
   final String appointmentId;
@@ -22,27 +23,179 @@ class ViewAppointmentScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('View Appointment'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Date: ${date['year']}-${date['month']}-${date['day']}',
-              style: const TextStyle(fontSize: 18),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 69, 167, 83)
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Icon(
+                            Icons.calendar_month_outlined,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Center(
+                    child: Text(
+                      'Hi, you have got an appointment, is it suitable for you?',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  FutureBuilder<String>(
+                    future: DoctorServices().getDoctorName(doctorId),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      }
+                      return ListTile(
+                        leading: Container(
+                          width: 45,
+                          height: 45,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(255, 123, 203, 247), // Változtatható szín
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.person, color: Colors.white),
+                          ),
+                        ),
+                        title: Text(snapshot.data ?? "Unknown", style: const TextStyle(fontSize: 18),),
+                      );
+                    },
+                  ),
+                  FutureBuilder<String>(
+                    future: DoctorServices().getDoctorAddress(doctorId),
+                    builder: (context, addressSnapshot) {
+                      if (addressSnapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (addressSnapshot.hasError) {
+                        return Text('Error: ${addressSnapshot.error}');
+                      }
+                      return ListTile(
+                        leading: Container(
+                          width: 45,
+                          height: 45,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(255, 235, 164, 58), 
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.location_on, color: Colors.white),
+                          ),
+                        ),
+                        title: Text(addressSnapshot.data ?? "Unknown Address", style: const TextStyle(fontSize: 18)),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 125, 211, 75), // Változtatható szín
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.calendar_today, color: Colors.white),
+                      ),
+                    ),
+                    title: Text('Date: ${date['year']}-${date['month']}-${date['day']}', style: const TextStyle(fontSize: 18)),
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 209, 103, 128),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.access_time, color: Colors.white),
+                      ),
+                    ),
+                    title: Text('Time: $hourMinute', style: const TextStyle(fontSize: 18)),
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 184, 78, 203), // Változtatható szín
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.message, color: Colors.white),
+                      ),
+                    ),
+                    title: Text(message, style: const TextStyle(fontSize: 18)),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add logic for accept button
+                        },
+                        child: const Text('Accept'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add logic for decline button
+                        },
+                        child: const Text('Decline'),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  // Text input for decline reason
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Reason for Decline',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add logic for send respond button
+                    },
+                    child: const Text('Send Respond'),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              'Time: $hourMinute',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Message: $message',
-              style: const TextStyle(fontSize: 18),
-            ),
-            // Ide lehet még további adatokat megjeleníteni az appointment objektumból
-          ],
+          ),
         ),
       ),
     );
